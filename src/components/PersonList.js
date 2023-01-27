9a20366bfd893ba626be31918bebf888ffca912e
PimChar <pimnaree.char@gmail.com>
2023-01-27 16:00:04 +0700

    test-axios1

---
+++ src/components/PersonList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://localhost.8080/user`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {
+          this.state.persons
+            .map(person =>
+              <li key={person.id}>{person.name}</li>
+            )
+        }
+      </ul>
+    )
+  }
+}
