9b3fd5255c80e6580635b8892df1c4a5a783d99e
PimChar <pimnaree.char@gmail.com>
2023-01-30 22:09:03 +0700

    commit
    edit bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 //import PersonAdd from './components/PersonAdd';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <button type="button" class="btn btn-primary">Primary</button>
     </div>
   )
 }
