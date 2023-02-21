d568590bc8451b3f2958346ddcfd5fa1b1631d68
sakuro44 <kanokwan.hmon@gmail.com>
2023-02-22 05:33:40 +0700

    หน้าFor The Perfect Balance

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Homeone from "../components/homeone.component"
+import "./home.css";
+
+
+export default function Home() {
+  return (
+  <div className="Relax">
+        <Homeone/>
+        
+    </div> 
+  );
+}
