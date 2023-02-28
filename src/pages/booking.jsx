6414097012b8fd9a6db9322e432a06568c0a6208
PimChar <pimnaree.char@gmail.com>
2023-02-28 10:24:40 +0700

    commit
    booking complete

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import AppointList from "../components/appointList.component";
-import Onsen from "../assets/onsen.png"
+import Onsen from "../assets/onsen.png";
 import Menu from "../components/menu";
+import Two from "../components/hometwo";
 import "./booking.css";
 
 export default function Booking() {
@@ -13,17 +14,20 @@ export default function Booking() {
             <div className="container d-flex justify-content-start booking">
                 <img className="mx-5 px-5 my-5" src={Onsen} alt="pic" />
                 <marquee direction="left" className="cobookig" scrollamount="20">
-          cat japan onsen cat japan onsen cat japan onsen cat japan onsen cat
-          japan onsen cat japan onsen cat japan onsen cat japan onsen cat japan
-          onsencat japan onsen cat japan onsen cat japan onsencat japan onsen
-          cat japan onsen cat japan onsen
-        </marquee>
+                    cat japan onsen cat japan onsen cat japan onsen cat japan onsen cat
+                    japan onsen cat japan onsen cat japan onsen cat japan onsen cat japan
+                    onsencat japan onsen cat japan onsen cat japan onsencat japan onsen
+                    cat japan onsen cat japan onsen
+                </marquee>
                 <span style={{ fontSize: '32px', marginTop: '500px' }}>Onsen <br />
                     Thai massage</span>
             </div>
 
             <div className="compo" style={{ marginTop: '4rem' }}>
                 <AppointList />
+            </div>
+            <div className="footer">
+                <Two />
             </div>
         </div>
     )
